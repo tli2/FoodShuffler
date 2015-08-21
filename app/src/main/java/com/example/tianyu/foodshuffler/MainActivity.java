4d9d57a305b8a8d01b385e86fa01e35aa4692c0e
Billy Zhu <zyx.billy@gmail.com>
2015-08-21 09:29:58 -0400

    add toolbar to Main page. add navigation drawer to Main page.

--- app/src/main/java/com/example/tianyu/foodshuffler/MainActivity.java
+++ app/src/main/java/com/example/tianyu/foodshuffler/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.tianyu.foodshuffler;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -21,6 +24,17 @@ public class MainActivity extends AppCompatActivity {
         //This simple UI consists of a TextView for displaying results or error messages and a button to initiate shuffle action
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Toolbar toolbar = (Toolbar) findViewById(R.id.main_toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setNavigationIcon(R.drawable.ic_menu_black_36dp);
+        final DrawerLayout drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
 
         final ProgressBar pb = (ProgressBar) findViewById(R.id.main_progress_bar);
         final MainActivity mainActivity = this;
